87ac5acfd0778c5b862e56d91967d861707d5a50
thengthon <thon.theng@student.passerellesnumeriques.org>
2021-05-21 00:37:06 +0700

    login and register

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
 const PORT = 5000;
 app.listen(process.env.PORT || PORT, () => console.log("Server is running...!"))
 
 app.use(express.static("frontend"));
+app.use(express.json());
+app.use(express.urlencoded());
+
+
+app.get("/getUsers", (req, res) => {
+    let users = fs.readFileSync("./dataServer/users.json");
+    res.send(users);
+})
+
+app.post("/addNewUser", (req, res) => {
+    let newUser = req.body;
+    let users = JSON.parse(fs.readFileSync("./dataServer/users.json"));
+    users.push(newUser);
+    fs.writeFileSync("./dataServer/users.json", JSON.stringify(users));
+    res.send("Add new user successfully...!");
+})
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,34 +1,84 @@
-function clearLogin(){
-    
+
+// _______CONSTANT VARIABLES____________________________________
+const URL = "http://localhost:5000";
+
+let preDisplay = document.querySelector(".login");
+
+function hideShow(pre, next, display){
+    pre.style.display = "none";
+    next.style.display = display;
+    preDisplay = next;
 }
 
-function clearRegister(){
+let goRegister = document.querySelector(".goRegister");
+goRegister.addEventListener("click", () => {
+    let register = document.querySelector(".register");
+    hideShow(preDisplay, register, "block");
+});
+
+let backBtn = document.querySelector(".backBtn");
+backBtn.addEventListener("click", () => {
+    let login = document.querySelector(".login");
+    hideShow(preDisplay, login, "block");
+});
+
+let exitBtn = document.querySelector(".exitBtn");
+exitBtn.addEventListener("click", () => {
+    let exit = document.querySelector("body");
+    exit.style.display = "none";
+});
+
+// ----------------------------------------------------------------
+function login(){
+    let username = document.querySelector("#fullName").value;
+    let password = document.querySelector("#pwd").value;
     
+    if (username !== "" && password !== ""){
+        axios.get(URL + "/getUsers").then((response) => {
+            let users = response.data;
+            let isValidUser = false;
+            for (let user of users){
+                usernameServer = user.name.username;
+                passwordServer = user.password;
+                if (username === usernameServer && password === passwordServer){
+                    let goUserPage = document.querySelector(".userPage");
+                    preDisplay = document.querySelector(".authentication");
+                    hideShow(preDisplay, goUserPage, "block");
+                }
+            }
+        })
+    }
 }
-function clearLogin(){
+
+let loginBtn = document.querySelector(".loginBtn");
+loginBtn.addEventListener("click", login);
+
+// ----------------------------------------------------------------
+function register(){
+    let fName = document.querySelector("#fName").value;
+    let lName = document.querySelector("#lName").value;
+    let email = document.querySelector("#email").value;
+    let password = document.querySelector("#registerPwd").value;
+    let confirmPwd = document.querySelector("#confirmPwd").value;
     
+    let isValidData = ((fName !== "") && (lName !== "") && (email !== "") && (password !== "") && (confirmPwd !== "") && (password === confirmPwd));
+    if (isValidData){
+        let newUser = {
+            "name" : {
+                "firstName" : fName,
+                "lastName" : lName,
+                "username" : fName + lName
+            },
+            "password" : password,
+            "email" : email,
+            "conversations" : []
+        };
+
+        axios.post(URL + "/addNewUser", newUser).then((response) => {
+            console.log(response.data);
+        });
+    }
 }
-function clearLogin(){
-    
-}
 
-let preDisplay = document.querySelector("#login");
-
-
-let clearLBtn = document.querySelector("#clearLogin");
-clearLBtn.addEventListener("click", clearLogin);
-
-let clearRBtn = document.querySelector("#clearRegister");
-clearRBtn.addEventListener("click", clearLogin);
-
-let loginBtn = document.querySelector("#loginBtn");
-loginBtn.addEventListener("click", clearLogin);
-
-let RegisterBtn = document.querySelector("#registerBtn");
-clearLBtn.addEventListener("click", clearLogin);
-
-let exitBtn = document.querySelector("#exitBtn");
-clearLBtn.addEventListener("click", clearLogin);
-
-let backBtn = document.querySelector("#backBtn");
-clearLBtn.addEventListener("click", clearLogin);
+let RegisterBtn = document.querySelector(".registerBtn");
+RegisterBtn.addEventListener("click", register);
