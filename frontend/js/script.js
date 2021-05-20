8b4870c31c97f6870a12aa25cebe0f2439edccef
chum-ch <yoeurnchum@gmail.com>
2021-05-20 21:12:57 +0700

    secoud

---
+++ frontend/js/script.js
@@ -0,0 +1,34 @@
+function clearLogin(){
+    
+}
+
+function clearRegister(){
+    
+}
+function clearLogin(){
+    
+}
+function clearLogin(){
+    
+}
+
+let preDisplay = document.querySelector("#login");
+
+
+let clearLBtn = document.querySelector("#clearLogin");
+clearLBtn.addEventListener("click", clearLogin);
+
+let clearRBtn = document.querySelector("#clearRegister");
+clearRBtn.addEventListener("click", clearLogin);
+
+let loginBtn = document.querySelector("#loginBtn");
+loginBtn.addEventListener("click", clearLogin);
+
+let RegisterBtn = document.querySelector("#registerBtn");
+clearLBtn.addEventListener("click", clearLogin);
+
+let exitBtn = document.querySelector("#exitBtn");
+clearLBtn.addEventListener("click", clearLogin);
+
+let backBtn = document.querySelector("#backBtn");
+clearLBtn.addEventListener("click", clearLogin);
